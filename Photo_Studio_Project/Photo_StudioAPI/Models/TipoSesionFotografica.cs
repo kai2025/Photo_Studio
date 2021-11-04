4ad31cff5ea631437a63cddd95267745379f832a
kai2025 <ing.sis.ameliav@gmail.com>
2021-11-03 22:54:12 -0400

    API Models

---
+++ Photo_Studio_Project/Photo_StudioAPI/Models/TipoSesionFotografica.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace Photo_StudioAPI.Models
+{
+    [Table("Tipo_SesionFotografica")]
+    public partial class TipoSesionFotografica
+    {
+        public TipoSesionFotografica()
+        {
+            Reservas = new HashSet<Reserva>();
+        }
+
+        [Key]
+        [Column("ID_Tipo")]
+        public int IdTipo { get; set; }
+        [Required]
+        [Column("Descripción_Sesion")]
+        [StringLength(100)]
+        public string DescripciónSesion { get; set; }
+
+        [InverseProperty(nameof(Reserva.TipoSesionNavigation))]
+        public virtual ICollection<Reserva> Reservas { get; set; }
+    }
+}
